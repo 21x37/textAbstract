2f9d21156c6fd203a03db616ff1ef6b127e88263
21x37 <tylermok@outlook.com>
2019-04-25 20:17:30 -0700

    Bug Fixes #1 🦏

--- textAbstract.js
+++ textAbstract.js
@@ -1,13 +1,15 @@
 module.exports = function textAbstract(text, length) {
     try {
+        if (text == null) {
+            return "";
+        };
         if (text.length <= length) {
             return text;
         };
         text = text.substring(0, length);
-        let last = text.lastIndexOf(" ");
-        text = text.substring(0, last);
-        return text + "..."; 
-    } catch(e) {
-        console.log('Please make sure you provided a string, and a length to textAbstract.')
+        text = text.substring(0, text.length);
+        return text + "...";
+    } catch (e) {
+        console.log(e,'Make sure you provided a string and length to textAbstract.');
     };
 };
